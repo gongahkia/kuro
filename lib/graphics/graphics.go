543e7fc5d968819ac693ac9187e912f6d47760e0
gongahkia <angryapplegravy@gmail.com>
2024-04-29 19:48:32 +0800

    done

---
+++ lib/graphics/graphics.go
@@ -0,0 +1,13 @@
+// FUA
+	// add logic for rendering the world by receiving fixed coordinates
+	// allow for rendering different layers to enable dithering lighting for the player character and how such lighting interacts with walls
+
+package graphics
+
+import (
+	"fmt"
+)
+
+func Draw() {
+	fmt.Println("Drawing world")
+}
--- src/main.go
+++ src/main.go
@@ -1,11 +1,18 @@
+// FUA
+    // add character controller under lib in a further directory and allow for interaction between keypress and the character controller
+    // allow rendering of player to the screen
+    // figure out how to render lighting
+
 package main
 
 import (
     "fmt"
-    "kuro/lib"
+    "kuro/lib/utils"
+    "kuro/lib/graphics"
 )
 
 func main() {
     utils.Test()
+    graphics.Draw()
     fmt.Println(utils.ReadKeypress())
 }
